2e84879ff10453d3fa8708b06ce9476bc7b927c1
Spike_Projects <msm74588@gmail.com>
2024-01-06 10:08:39 +0530

    added tailwind via svelte-add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    // npx svelte-add@latest tailwindcss
+    // https://youtu.be/w2q9caYXgkg?si=d2T1vODeq5bVn55w&t=208
+</script>
+
+<h1 class="text-3xl font-bold text-blue-600">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
