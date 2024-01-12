6b9563c305d317ea2a5780067bda47bde0210bae
Spike_Projects <msm74588@gmail.com>
2024-01-13 00:12:47 +0530

    Refactored Search area

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -6,6 +6,8 @@
 
 	import '@fontsource-variable/lexend-deca';
 
+	import { MagnifyingGlass } from 'phosphor-svelte';
+
 	import { cn } from '$lib/utils/style';
 
 	import { createTabs, melt } from '@melt-ui/svelte';
@@ -24,7 +26,7 @@
 	export { className as class };
 
 	const triggers = [
-		{ id: 'tab-1', title: 'Bookmarked Videos'},
+		{ id: 'tab-1', title: 'Bookmarked Videos' },
 		{ id: 'tab-2', title: 'Watch Later' }
 	];
 
@@ -45,45 +47,60 @@
 				<h1 class="font-title text-lg">Bookmarked Channels:</h1>
 			</div>
 		</div>
-		<div class="flex-grow bg-neutral-200 border-x-2 border-neutral-400" id="main">
-			<div use:melt={$root} class={cn('flex flex-col overflow-hidden w-full h-full  data-[orientation=vertical]:flex-row', className,)}>
-
+		<div class="flex-grow border-x-2 border-neutral-400 bg-neutral-200" id="main">
+			<div
+				use:melt={$root}
+				class={cn(
+					'flex h-full w-full flex-col overflow-hidden  data-[orientation=vertical]:flex-row',
+					className
+				)}
+			>
 				<!-- List of tabs -->
-				<div use:melt={$list} class="flex data-[orientation=vertical]:flex-col data-[orientation=vertical]:border-r overflow-x-auto justify-center border-b-2 border-neutral-400" aria-label="Videos tab">
+				<div
+					use:melt={$list}
+					class="flex justify-center overflow-x-auto border-b-2 border-neutral-400 data-[orientation=vertical]:flex-col data-[orientation=vertical]:border-r"
+					aria-label="Videos tab"
+				>
 					{#each triggers as triggerItem}
-					<button use:melt={$trigger(triggerItem.id)} class="trigger relative  tab-button">
-					  {triggerItem.title}
-					  {#if $value === triggerItem.id}
-						<div
-						  in:send={{ key: 'trigger' }}
-						  out:receive={{ key: 'trigger' }}
-						  class="absolute bottom-1 left-1/2 h-1 w-6 -translate-x-1/2 rounded-full bg-red-700"
-						/>
-					  {/if}
-					</button>
-				  {/each}
+						<button use:melt={$trigger(triggerItem.id)} class="trigger tab-button relative">
+							{triggerItem.title}
+							{#if $value === triggerItem.id}
+								<div
+									in:send={{ key: 'trigger' }}
+									out:receive={{ key: 'trigger' }}
+									class="absolute bottom-1 left-1/2 h-1 w-6 -translate-x-1/2 rounded-full bg-red-700"
+								/>
+							{/if}
+						</button>
+					{/each}
 				</div>
-				<div use:melt={$content('tab-1')}>
-					TaB 1
-				</div>
+				<div use:melt={$content('tab-1')}>TaB 1</div>
 				<div use:melt={$content('tab-2')}>
-					<h1 class="text-3xl ">HELLO WORLD, Tab 2</h1>
+					<h1 class="text-3xl">HELLO WORLD, Tab 2</h1>
 				</div>
 			</div>
-
-
 		</div>
 		<div class="w-[400px] bg-[#C7BFBF]" id="right-sidebar">
 			<!-- RIGHT SIDEBAR -->
 			<div class="w-full px-4 pt-4" id="search-field">
-				<form action="/"  >
-					<label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-					<div class="relative">
-						<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-non">
-							<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="#97ebd1" viewBox="0 0 256 256"><path d="M232.49,215.51,185,168a92.12,92.12,0,1,0-17,17l47.53,47.54a12,12,0,0,0,17-17ZM44,112a68,68,0,1,1,68,68A68.07,68.07,0,0,1,44,112Z"></path></svg>
+				<form action="/">
+					<div class="flex flex-row ring-red-500 has-[:focus]:ring-2 bg-neutral-50 rounded transition-[0.3s] ring-offset-2 ">
+						<label
+							for="search"
+							class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">Search</label
+						>
+
+						<div class="pointer-events-non inset-y-0 start-0 flex items-center ps-3">
+							<MagnifyingGlass color="#AE2983" weight="bold" size="{30}"/>
 						</div>
+						<input
+							required
+							id="search"
+							type="search"
+							placeholder="Search"
+							class="w-full px-3 py-2 font-lexend  placeholder:text-neutral-600 focus:outline-none bg-transparent"
+						/>
 					</div>
-					<input required id="search" type="search" placeholder="Search" class="block w-full focus:outline-none focus:ring-2 ring-red-500 ring-offset-2 px-3 py-2 font-lexend placeholder:text-neutral-600 rounded">
 				</form>
 			</div>
 		</div>
@@ -92,43 +109,43 @@
 
 <style lang="postcss">
 	.trigger {
-	transition: 0.3s;
+		transition: 0.3s;
 
-	font-family: 'Lexend Deca Variable', sans-serif;
-	font-weight: 500;
+		font-family: 'Lexend Deca Variable', sans-serif;
+		font-weight: 500;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
- 
-    cursor: default;
-    user-select: none;
- 
-    border-radius: 0;
-    background-color: theme(colors.neutral.300);
- 
-    color: theme(colors.neutral.500);
-    
-    line-height: 1;
- 
-    flex: 1;
-    height: theme(spacing.12);
-    padding-inline: theme(spacing.2);
- 
-    &:focus {
-      position: relative;
-    }
- 
-    &:focus-visible {
-      @apply z-10 ring-2;
-    }
- 
-    &[data-state='active'] {
-      @apply focus:relative;
-      background-color: white;
-      background-color: theme(colors.neutral.200);
-	  color: theme(colors.red.700);
-	  font-weight: 600;
-    }
-  }
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		cursor: default;
+		user-select: none;
+
+		border-radius: 0;
+		background-color: theme(colors.neutral.300);
+
+		color: theme(colors.neutral.500);
+
+		line-height: 1;
+
+		flex: 1;
+		height: theme(spacing.12);
+		padding-inline: theme(spacing.2);
+
+		&:focus {
+			position: relative;
+		}
+
+		&:focus-visible {
+			@apply z-10 ring-2;
+		}
+
+		&[data-state='active'] {
+			@apply focus:relative;
+			background-color: white;
+			background-color: theme(colors.neutral.200);
+			color: theme(colors.red.700);
+			font-weight: 600;
+		}
+	}
 </style>
